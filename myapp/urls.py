2e0a664758867ff81dfb093287a5656ffd937db5
biancaaraujo15 <155780178+biancaaraujo15@users.noreply.github.com>
2024-03-13 17:37:35 +0000

    class 03/07

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
   path("",views.home, name="home"),
   path("about/",views.about, name="about"),
   path('function/', views.function_view, name='function_view'),
-  path('class/', views.ClassView.as_view(), name='class_view'),
+  path('class/', views.ClassView.as_view(), name='class_view'), 
+  path('theme/', views.ThemeView.as_view(), name='theme'),
 ]
 
