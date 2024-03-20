78a1a6c729ef2337597dc5d1ac730c857ec35dc8
biancaaraujo15 <155780178+biancaaraujo15@users.noreply.github.com>
2024-03-20 18:20:50 +0000

    aqui funciona web view pero no aparece la informacion pero la pagina funciona

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Invention
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Invention)
+
+
--- myapp/views.py
+++ myapp/views.py
@@ -6,7 +6,10 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.http import JsonResponse
 from .default_data import load_default_data
-
+from django.views.generic import ListView
+from django.urls import reverse_lazy
+from .models import Invention
+from django.views.generic import DetailView
 
 # Create your views here.
 def home(request):
@@ -59,6 +62,18 @@ class ThemeView(BaseView):
       return response
 
   def load_default_data_view(request):
-      load_default_data(None)  # Call the load_default_data function
+      load_default_data()  # Call the load_default_data function
       return JsonResponse({'status': 'success'})
+  
+  
+class InventionListView(ListView):
+    model = Invention
+    template_name = 'invention_list.html'
+    context_object_name = 'inventions'
+  
+
+class InventionDetailView(DetailView):
+    model = Invention
+    template_name = 'invention_view.html'
+    context_object_name = 'invention'
 
